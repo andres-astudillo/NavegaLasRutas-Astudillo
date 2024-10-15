f8d49719a5ed631b8fac2ce8530394825e4c5dc0
Andrés Astudillo <andresdiastullo@gmail.com>
2024-10-15 10:50:51 -0300

    Correcciones

---
+++ libreria/src/Components/assets/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+	{
+		id: 1,
+		img: 'src/Components/assets/img/render-libro-familia-2.webp',
+		nameProduct: 'Mejor una Familia',
+		autor: 'Fabian Ruiz',
+		price: 1800,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'src/Components/assets/img/render-libro-familia-21.webp',
+		nameProduct: '¡Bienaventurados!',
+		autor: 'Fabian Ruiz',
+		price: 1200,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'src/Components/assets/img/render-tormentas-financieras.webp',
+		nameProduct: 'Tormentas Financieras',
+		autor: 'Fabian Ruiz',
+		price: 1300,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'src/Components/assets/img/render-una-sola-pasion.webp',
+		nameProduct: 'Una Sola Pasión',
+		autor: 'Fabian Ruiz',
+		price: 1500,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'src/Components/assets/img/render-padres-de-valor.webp',
+		nameProduct: 'Padres de Valor',
+		autor: 'Fabian Ruiz',
+		price: 2000,
+		quantity: 1,
+	},
+];
