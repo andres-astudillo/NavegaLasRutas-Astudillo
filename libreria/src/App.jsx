48e5b7ee78afbbf1925aa40a9740744539c1dcc6
Andrés Astudillo <andresdiastullo@gmail.com>
2024-10-15 11:07:13 -0300

    Fix

--- libreria/src/App.jsx
+++ libreria/src/App.jsx
@@ -1,32 +1,33 @@
+import { Route, Routes } from "react-router-dom";
 import { useState } from 'react';
-import { Header } from './components/Header';
-import { ProductList } from './components/ProductList';
+import { Header } from './Components/Header';
+import { ProductList } from './Components/ProductList';
 
 function App() {
-	const [allProducts, setAllProducts] = useState([]);
-	const [total, setTotal] = useState(0);
-	const [countProducts, setCountProducts] = useState(0);
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
 
-	return (
-		<>
-			<Header
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-			/>
-			<ProductList
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-			/>
-		</>
-	);
+  return (
+    <>
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+    </>
+  );
 }
 
 export default App;
